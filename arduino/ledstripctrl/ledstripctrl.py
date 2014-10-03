1add4d1710a8a39551e1c2dd2e6c570ed4a90df3
Oren Livne <oren.livne@gmail.com>
2014-10-03 07:20:50 -0500

    playing with rgb color displays.

--- arduino/ledstripctrl/ledstripctrl.py
+++ arduino/ledstripctrl/ledstripctrl.py
@@ -14,56 +14,94 @@ import matplotlib.pyplot as plt
 from time import sleep
 from MagicCube.code.cube_interactive import Cube
 
-'''A generic controller of a digital LED strip via a USB stream.''' 
 class LedStripContoller(object):
-    def __init__(self, serial_stream, led_count):
+    '''A generic controller of a digital LED strip via a USB stream.''' 
+    def __init__(self, serial_stream, led_count, debug=False):
         self._serial_stream = serial_stream
         self._led_count = led_count
-            
-    def blink_test(self, num_blinks):
-        for _ in xrange(num_blinks):
-            self.turn_off()
-            #self._show()
-            sleep(0.5)
-            
-            self.set_uniform_color('FFFFFF')
-            #self._show()
-            sleep(0.5)
+        self._debug = debug
         self.turn_off()
-    
-    # Set a uniform color to all LEDs.
+            
     def set_uniform_color(self, color):
+        '''Set a uniform color to all LEDs.'''
         for i in xrange(self._led_count):
             self._led_set(i, color)
         self._show()
 
-    # Turn the strip off.
     def turn_off(self):
+        '''Turn the strip off.'''
         self.set_uniform_color('000000')
     
-    def _clear_read_buffer(self):
-        print self._serial_stream.readline().strip()
+    def blink_test(self, num_blinks, color='FFFFFF'):
+        for _ in xrange(num_blinks):
+            self.turn_off()
+            # self._show()
+            sleep(0.5)
+            
+            self.set_uniform_color(color)
+            # self._show()
+            sleep(0.5)
+        self.turn_off()
+
+    def rainbow_cycle(self, num_cycles=5, delay=0):
+        '''Makes a rainbow wheel that is equally distributed along the chain.'''
+        num_colors = 384
+        for j in xrange(1):  # xrange(num_colors * num_cycles):  # n cycles of all 384 colors in the wheel
+            for i in xrange(self._led_count):
+                # tricky math! we use each pixel as a fraction of the full 384-color wheel
+                # (thats the i / strip.numPixels() part)
+                # Then add in j which makes the colors go around per pixel
+                # the % 384 is to make the wheel cycle around
+                r, g, b = self._wheel(((i * num_colors / self._led_count) + j) % num_colors)
+                self._led_set(i, '%02X%02X%02X' % (r, g, b))
+            self._show()
+            time.sleep(delay)
+
+    def _flush_read_buffer(self):
+        data_from_arduino = self._serial_stream.readline().strip()
+        if self._debug:
+            print data_from_arduino
     
     def _show(self):
         self._serial_stream.write('FF\n')
-        print self._serial_stream.readline()
+        self._flush_read_buffer()
     
     def _led_set(self, led, rgb):
         outval = '%02X%0s' % (led, rgb)
-        print outval
+        print 'Setting LED %d to %s (outval=%s)' % (led, rgb, outval)
         self._serial_stream.write('%s\n' % outval)
-        print self._serial_stream.readline()
+        self._flush_read_buffer()
 
+    '''Returns an RGB color value for a color identifier between 0 and 384.
+    Colors are a transition r - g -b - back to r.'''
+    def _wheel(self, wheel_pos):
+        q = wheel_pos / 128
+        if q == 0:
+            r = 127 - wheel_pos % 128  # Red down
+            g = wheel_pos % 128  # green up
+            b = 0  # blue off
+        elif q == 1:
+            g = 127 - wheel_pos % 128  # green down
+            b = wheel_pos % 128  # blue up
+            r = 0  # red off
+        elif q == 2:
+            b = 127 - wheel_pos % 128  # blue down 
+            r = wheel_pos % 128  # red up
+            g = 0  # green off
+        return r, g, b
+    
 '''Integrates MagicCube with the LED strip. Sends the cube state
 to the LED strip upon a call to send_cube_state().'''
 class CubeLedStripContoller(LedStripContoller):
-    def __init__(self, serial_stream, led_count, face_colors):
-        LedStripContoller.__init__(self, serial_stream, led_count)
+    def __init__(self, serial_stream, led_count, face_colors, debug=False):
+        LedStripContoller.__init__(self, serial_stream, led_count, debug=debug)
         self.face_colors = map(lambda x: (x[1:] if x.startswith('#') else x).upper(), face_colors)
 
     def send_cube_state(self, sticker_color_id):
+        print face_colors
         print ' '.join(repr(y) for y in sticker_color_id)
         for i in xrange(min(led_count, len(sticker_color_id))):
+            print 'Sticker %d: color %s' % (i, self.face_colors[sticker_color_id[i]])
             self._led_set(i, self.face_colors[sticker_color_id[i]])
             self._show()
         
@@ -91,7 +129,16 @@ if __name__ == '__main__':
         time.sleep(2)
         print 'Starting controller'
         controller = CubeLedStripContoller(stream, led_count, face_colors[:2 * N])
-        #controller.blink_test(N)
+        # Test how face colors appear on the LED strip.
+#         for x in face_colors[:6]:
+#             controller.set_uniform_color(x[1:])
+#             time.sleep(3)
+        controller.rainbow_cycle(5, 0)
+        time.sleep(2)
+        controller.turn_off()
+                
+        # input('Press ENTER to continue.')
+        # controller.blink_test(N, color='#ff0000')
     
         # Bring up the cube visualization. Add a call back that sends
         # the cube state to the Arduino.
